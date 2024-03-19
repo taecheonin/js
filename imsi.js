1e16c286729311f45646cc0d99362c3a9e4d7258
Taecheonin <taecheonin@nate.com>
2024-03-19 10:12:52 +0000

    개발자테스트

---
+++ imsi.js
@@ -0,0 +1,7 @@
+console.log("현재 페이지의 전체 URL: " + window.location.href);
+console.log("호스트 이름과 포트 번호(있는 경우): " + window.location.host);
+console.log("호스트 이름: " + window.location.hostname);
+console.log("포트 번호: " + window.location.port);
+console.log("경로 부분: " + window.location.pathname);
+console.log("쿼리스트링: " + window.location.search);
+console.log("프래그먼트 식별자: " + window.location.hash);
